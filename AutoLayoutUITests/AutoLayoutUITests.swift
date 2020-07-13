a075ec2b87b4bc14f764e38397a7ee5ab01ae11a
Den Bagus <goodfe@yahoo.com>
2020-07-13 13:57:34 +0700

    Initial Commit

---
+++ AutoLayoutUITests/AutoLayoutUITests.swift
@@ -0,0 +1,34 @@
+//
+//  AutoLayoutUITests.swift
+//  AutoLayoutUITests
+//
+//  Created by Den Bagus on 13/07/20.
+//  Copyright © 2020 Den Bagus. All rights reserved.
+//
+
+import XCTest
+@testable import AutoLayoutUI
+
+class AutoLayoutUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
